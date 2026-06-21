ea9463d45b05b2b476215782e8e62eafc1b03ddf
jordijoangimenez <jordijoangimenez@gmail.com>
2026-06-21 23:09:34 +0200

    Add 5G-MAG license headers to all source files

--- ConnectivityQualityManagement/dedicated-networks-portal/routes/areas.js
+++ ConnectivityQualityManagement/dedicated-networks-portal/routes/areas.js
@@ -1,3 +1,13 @@
+/*
+License: 5G-MAG Public License (v1.0)
+Author: Jordi Joan Gimenez
+Copyright: (C) 2026 5G-MAG Association
+
+For full license terms please see the LICENSE file distributed with this
+program. If this file is missing then the license can be retrieved from
+https://hub.5g-mag.com/Getting-Started/OFFICIAL_5G-MAG_Public_License_v1.0.pdf
+*/
+
 const express = require('express');
 const { apiRequest } = require('../services/apiService');
 const router = express.Router();
