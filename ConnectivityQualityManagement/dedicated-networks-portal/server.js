13c79c9856e6e14349546f53dd53a5dd939e8400
jordijoangimenez <jordijoangimenez@gmail.com>
2026-06-30 18:40:13 +0200

    feat(DedicatedNetworksPortal): local Leaflet bundle, adaptive polling, profile modal, alerts
    - Bundle Leaflet 1.9.4 and leaflet-control-geocoder locally in public/lib/
      removing CDN dependency and fixing marker icon path
    - Replace embedded base64 logo with /5gmag.png file reference
    - Update 5G-MAG logo
    - Fix DL/UL arrow characters in map popup
    - Make network profile tag clickable to open profile modal (home tab)
    - Expiry warning alert with dynamic threshold on home tab and Networks panel
    - ACTIVATED status transition alert on home tab
    - Adaptive 5s/30s polling on both tabs: 5s while transitioning or near/past
      expiry, 30s once stable; continues at 5s until TERMINATED after expiry
    - Fix ACTIVATED alert missed on first poll after homeStartPolling

--- ConnectivityQualityManagement/dedicated-networks-portal/server.js
+++ ConnectivityQualityManagement/dedicated-networks-portal/server.js
@@ -1,13 +1,3 @@
-/*
-License: 5G-MAG Public License (v1.0)
-Author: Jordi Joan Gimenez
-Copyright: (C) 2026 5G-MAG Association
-
-For full license terms please see the LICENSE file distributed with this
-program. If this file is missing then the license can be retrieved from
-https://hub.5g-mag.com/Getting-Started/OFFICIAL_5G-MAG_Public_License_v1.0.pdf
-*/
-
 require('dotenv').config();
 
 const express = require('express');
